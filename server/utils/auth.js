7ac7b7ea0f372dcb86c2e03f699cd5ec2da7428d
Joshua <Joshua.soto@live.com>
2024-01-30 16:15:51 -0600

    added to auth and added code for res and typedef

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -31,4 +31,13 @@ module.exports = {
 return req;
 
   },
+
+signToken: function ({ username, email, _id }) {
+  const payload = { username, email, _id };
+
+  return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
+
+  },
 };
+
+
